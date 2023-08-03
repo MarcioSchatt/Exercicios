dbec878ccb758cfc7cc0f44b8eb374cd75987b49
Marcio Schatt <jrschatt12@hotmail.com>
2023-08-02 23:21:40 -0300

    Exercícios

--- Python/EstruturaDeRepeticao/exercicio_10.py
+++ Python/EstruturaDeRepeticao/exercicio_10.py
@@ -8,7 +8,7 @@ numero2 = int(input(f"Digite o 2º número: "))
 print(f"Os números inteiros entre {numero1} e {numero2} são:", end=" ")
 
 if numero1 > numero2:
-    for i in range(numero1, numero2, -1):
+    for i in range(numero1-1, numero2, -1):
         print(i, end=" ")
 else:
     for i in range(numero1, numero2):
